3f9922781ef313271467677a25897bbb7b0acfbc
Jared Hanson <jaredhanson@gmail.com>
2012-11-23 22:21:44 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,29 @@
+define(['exports',
+        './lib/stream'],
+function(exports, Stream) {
+  
+  var getUserMedia = navigator.getUserMedia ||
+                     navigator.webkitGetUserMedia ||
+                     navigator.mozGetUserMedia ||
+                     navigator.msGetUserMedia;
+
+  function get(constraints, cb) {
+    if (typeof constraints == 'function') {
+      cb = constraints;
+      constraints = { audio: true, video: true };
+    }
+    
+    
+    getUserMedia.call(navigator, constraints,
+      function(s) { // success callback
+        console.log('media stream success')
+        cb(null, new Stream(s));
+      },
+      function(err) { // error callback
+        console.log('media stream error')
+        cb(err);
+      });
+  }
+
+  exports.get = get;
+});
